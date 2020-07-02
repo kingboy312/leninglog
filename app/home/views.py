44247163032922cc504cc5b5582f652e95e209b7
yqqiu <qyqqiu@163.com>
2020-07-02 14:42:57 +0800

    git add .

--- app/home/views.py
+++ app/home/views.py
@@ -12,7 +12,7 @@ def index():
 @home.route('/topic/')
 def topic():
     topic = Topic.query.filter_by()
-    return render_template("home/topics.html",topics = topic)
+    return render_template("home/topics.html",topics = topic,Enty = Empty,topicid = None)
 @home.route('/leninglog/')
 def bat():
     return render_template("home/bat.html")
