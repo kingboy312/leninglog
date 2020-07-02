09f4af95ad66e939ea2fb87a80e27884b17182c3
yqqiu <qyqqiu@163.com>
2020-07-02 15:13:58 +0800

    git add .

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from config import config,DevelopmentConfig
 def create_app():
     app_ = DevelopmentConfig()
     app = app_.app
+    app.config = config
     from app.home import home as home_blueprint
     app.register_blueprint(home_blueprint)
     return app
