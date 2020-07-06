45b25e1938defbdb20a254dfcdfa543e9ec4488e
yqqiu <qyqqiu@163.com>
2020-07-06 17:02:00 +0800

    git add .

--- app/home/forms.py
+++ app/home/forms.py
@@ -28,3 +28,58 @@ class newentryforme(FlaskForm):
             "class": "btn btn-primary",
         }
     )
+class registerforme(FlaskForm):
+    username = StringField(
+        validators=[
+            DataRequired("用户名不能为空！"),
+        ],
+        description="用户名",
+        render_kw={
+            "placeholder": "请输入用户名！",
+        }
+    )
+    email = StringField(
+        validators=[
+            DataRequired("邮箱不能为空！"),
+            Email("邮箱格式不正确！")
+        ],
+        description="邮箱",
+        render_kw={
+            "type": "email",
+            "placeholder": "请输入邮箱！",
+        }
+    )
+    pwd = PasswordField(
+        validators=[
+            DataRequired("密码不能为空！")
+        ],
+        description="密码",
+        render_kw={
+            "placeholder": "请输入密码！",
+        }
+    )
+    repwd = PasswordField(
+        validators=[
+            DataRequired("请输入确认密码！"),
+            EqualTo('pwd', message="两次密码不一致！")
+        ],
+        description="确认密码",
+        render_kw={
+            "placeholder": "请输入确认密码！",
+        }
+    )
+    submit = SubmitField(
+        '注册',
+        render_kw={
+            "class": "btn btn-primary",
+        }
+    )
+     def validate_email(self, field):
+        """
+        检测注册邮箱是否已经存在
+        :param field: 字段名
+        """
+        email = field.data
+        user = User.query.filter_by(email=email).count()
+        if user == 1:
+            raise ValidationError("邮箱已经存在！")
--- app/models.py
+++ app/models.py
@@ -1,22 +1,22 @@
 from .import SQLAlchemy,create_app
 from datetime import datetime
 db = SQLAlchemy(create_app())
-class Topic(db.Model):
-    __tablename__ = "topic"
-    id = db.Column(db.Integer, primary_key=True)
-    topic_s = db.Column(db.String(100)) 
-    enty_id = db.Column(db.Integer,db.ForeignKey('empty.id')) 
-    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
-    def __str__(self):
-        return '<topic %r>' % self.topic_s
 class User(db.Model):
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(1000))
     pwd = db.Column(db.String(100))
     email = db.Column(db.String(100), unique=True)
+    admin = db.Column(db.Integer,default=0)
     def __repr__(self):
         return '<User %r>' % self.name
+class Topic(db.Model):
+    __tablename__ = "topic"
+    id = db.Column(db.Integer, primary_key=True)
+    topic_s = db.Column(db.String(100))
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+    def __str__(self):
+        return '<topic %r>' % self.topic_s
 class Empty(db.Model):
     __tablename__ = "empty"
     id = db.Column(db.Integer, primary_key=True)
